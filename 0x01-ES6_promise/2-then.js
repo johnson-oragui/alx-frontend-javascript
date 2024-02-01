72dbcd533c2c9f8fbec01fd8a5a1ffa2b462108c
johnson-oragui <johnson-oragui@gmail.com>
2024-02-01 14:57:50 +0100

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,8 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({
-      status: 2000,
-      body: 'success',
-    }))
+    .then(() => (
+      { status: 200, body: 'success' }
+    ))
     .finally(() => {
       console.log('Got a response from the API');
     })
